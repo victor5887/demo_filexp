0cd3219d2ecbd77cb083bfe19908fbb546648ee1
Victor Krause <victor5887@gmail.com>
2020-06-24 23:16:48 -0300

    initial commit

---
+++ src/main/java/com/example/demo_filex/DemoFilexApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_filex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFilexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFilexApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_filex/DemoFilexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_filex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFilexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
